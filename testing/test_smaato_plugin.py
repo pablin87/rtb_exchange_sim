d5519d258ba21fd2a356f6c166dcd0a286d43ce0
Pablo Berrilio <pabloberrilio@gmail.com>
2015-07-28 10:59:19 -0300

    @MRP-1857 Simulator support the CDATA wrapper inside xml nodes.
    + It was already supported but i added a few tests to test it.

--- testing/test_smaato_plugin.py
+++ testing/test_smaato_plugin.py
@@ -22,6 +22,7 @@ instance" xsi:noNamespaceSchemaLocation="smaato_ad_v0.9.xsd" modelVersion="
 </imageAd>
 </ad>'''
 
+    adm_example2 = '''<ad xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='smaato_ad_v0.9.xsd' modelVersion='0.9'><imageAd><clickUrl><![CDATA[http://www.motrixi.com]]></clickUrl><imgUrl><![CDATA[http://media.crispadvertising.com/adframework/1.2/components/img/300x50.gif]]></imgUrl><width>320</width><height>50</height><beacons><beacon><![CDATA[http://54.208.75.195:12340/nurl/smaato/${AUCTION_ID}/${AUCTION_PRICE}?a=A&b=B&impid=${AUCTION_IMP_ID}]]></beacon></beacons></imageAd></ad>'''
     adm_bad_example1 = '''<ad xmlns:xsi="http://www.w3.org/2001/XMLSchema-
 instance" >
 <imageAd>
@@ -40,7 +41,13 @@ instance" >
         
         bad_adm = self.adm_bad_example1
         self.assertEquals( [ ], smt.extract_adm_impression_beacons(bad_adm))
-    
+        
+        # With CDATA
+        expected_beacons = set(["http://54.208.75.195:12340/nurl/smaato/${AUCTION_ID}/${AUCTION_PRICE}?a=A&b=B&impid=${AUCTION_IMP_ID}"])
+        adm = self.adm_example2
+        bcns = set(smt.extract_adm_impression_beacons(adm))
+        self.assertEquals(bcns, expected_beacons)
+        
     def testExtractImageUrl(self):
         adm = self.adm_example1
         smt = SmaatoPlugin()
@@ -50,7 +57,14 @@ instance" >
         
         bad_adm = self.adm_bad_example1
         self.assertEquals([], smt.extract_image_url(bad_adm))
-    
+        
+        # With CDATA
+        expected_imp = ["http://media.crispadvertising.com/adframework/1.2/components/img/300x50.gif"]
+        adm = self.adm_example2
+        imp = smt.extract_image_url(adm)
+        self.assertEquals(imp, expected_imp)
+        
+        
     def testExtractClickBeacon(self):
         adm = self.adm_example1
         smt = SmaatoPlugin()
@@ -60,6 +74,13 @@ instance" >
         
         bad_adm = self.adm_bad_example1
         self.assertEquals([], smt.extract_adm_click_beacons(bad_adm))
+        
+        # Case with CDATA
+        adm = self.adm_example2
+        expected_click = ["http://www.motrixi.com"]
+        click = smt.extract_adm_click_beacons(adm)
+        self.assertEquals(click, expected_click)
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
